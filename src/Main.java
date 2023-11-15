102106068bd3c09b956f14d3f8f4e8c9943ef9d5
patrick <contaoficail@gmail.com>
2023-11-15 20:44:49 -0300

    aula 4/5

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,10 @@ public class Main {
         cachorro1.peso = 5.55;
         cachorro1.temanhoDoRabo = 5;
 
-        //cachorro1.latir();
-        // System.out.println("O Cachorro Pegou Uma " + cachorro1.pegar());
-
         System.out.println("O Cachorro Esta " + cachorro1.interagir("vai dormir"));
         System.out.println("O Cachorro Esta " + cachorro1.interagir("carinho"));
+        System.out.println("O Cachorro Esta " + cachorro1.interagir("pisar na patinha"));
         System.out.println("O Cachorro Esta " + cachorro1.interagir("nada"));
-    }  u
+
+    }
 }
--- src/animais/cachorro.java
+++ src/animais/cachorro.java
@@ -2,32 +2,50 @@ package animais;
 
 public class cachorro {
 
+    //Attributes
+
     public String nome;
     public String cor;
     public int altura;
     public double peso;
     public int temanhoDoRabo;
+    public String estadoDeEspirito = "Neutro";
 
-    public String estadoDeEspirito;
 
-    public void comer(){}
+    //Metodos
 
-    public void latir(){
+    public void comer() {
+    }
+
+    public void latir() {
         System.out.println("AU AU");
     }
 
-    public String pegar(){
+    public String pegar() {
         return "Bolinha";
     }
-    public String interagir(String acao){
-        if (acao.equals("carinho")){
-            this.estadoDeEspirito = "feliz";
-        } else if (acao.equals("vai dormir")){
-            this.estadoDeEspirito = "bravo";
-        }else{
-            this.estadoDeEspirito = "neutro";
+
+    public String interagir(String acao) {
+
+        //enum (estudar)
+
+        switch (acao) {
+            case "carinho": this.estadoDeEspirito = "Feliz"; break;
+            case "vai dormir": this.estadoDeEspirito = "bravo"; break;
+            case "pisar na patinha" : this.estadoDeEspirito = "triste"; break;
+            default: this.estadoDeEspirito = "neutro";
         }
         return estadoDeEspirito;
 
     }
+    //        if (acao.equals("carinho")){
+//            this.estadoDeEspirito = "feliz";
+//        } else if (acao.equals("vai dormir")){
+//            this.estadoDeEspirito = "bravo";
+//        }else{
+//            this.estadoDeEspirito = "neutro";
+//        }
+//        return estadoDeEspirito;
+
+
 }
